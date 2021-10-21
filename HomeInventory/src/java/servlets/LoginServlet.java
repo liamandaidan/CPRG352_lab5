4f7d1c3d8ab80c2236d74faca2d5c5645e50f82e
liamandaidan <liammclaughlinis@gmail.com>
2021-10-20 21:00:38 -0600

    loginSession
    added the start of a session

--- HomeInventory/src/java/servlets/LoginServlet.java
+++ HomeInventory/src/java/servlets/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -21,6 +22,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
